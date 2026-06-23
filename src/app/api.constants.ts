6368fb9097ea6572cfd31f066c5793aa382a1018
Mehmet PAŞAOĞLU <mehpas@hotmail.com>
2026-06-23 22:50:58 +0300

    enviroment yapısı kuruldu

--- src/app/api.constants.ts
+++ src/app/api.constants.ts
@@ -1,4 +1,6 @@
-export const API_BASE = '/api';
+import { environment } from '../environments/environment';
+
+export const API_BASE = environment.apiUrl;
 
 export const ENDPOINTS = {
   KULLANICILAR: API_BASE + '/Kullanicilar'
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://englishlearning-api.onrender.com/api'
+};
